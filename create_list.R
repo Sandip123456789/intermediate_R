fe73721e183a5ed8911710538673e74e982dff99
Sandip chaudhary <sndp.c114@gmail.com>
2025-05-26 21:08:12 +0200

    Initial commit for learning path Intermediate R

---
+++ create_list.R
@@ -0,0 +1,23 @@
+# Vector with numerics from 1 up to 10
+my_vector <- 1:10 
+
+# Matrix with numerics from 1 up to 9
+my_matrix <- matrix(1:9, ncol = 3)
+
+# First 10 elements of the built-in data frame mtcars
+my_df <- mtcars[1:10,]
+
+# Adapt list() call to give the components names
+my_list <- list(vec = my_vector, mat = my_matrix, df = my_df)
+
+# Print out my_list
+my_list
+#*******************************************************************
+
+#Creating variables Movie, actors and reviews
+comments <- c("Best Horror Film I Have Ever Seen", "A truly brilliant and scary film from Stanley Kubrick", "A masterpiece of psychological horror")
+sources <- c("IMDb1", "IMDb2", "IMDb3")
+scores <- c(4.5, 4.0, 5.0)
+movie_title <- "The Shining"
+actors <- c( "Jack Nicholson", "Shelley Duvall", "Danny Lloyd", "Scatman Crothers", "Barry Nelson" )
+reviews_df <- data.frame(scores, sources, comments)
---
+++ relational_operators.R
@@ -0,0 +1,31 @@
+# Comparison of logicals
+TRUE == FALSE
+
+# Comparison of numerics
+-6 * 14 != 17-101
+
+# Comparison of character strings
+"useR" == "user"
+
+# Compare a logical with a numeric
+TRUE == 1
+
+# Comparison of numerics
+-6 * 5 + 2 >= -10+1
+
+# Comparison of character strings
+"raining" <= "raining dogs"
+
+# Comparison of logicals
+TRUE > FALSE
+
+# The social data has been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+views <- matrix(c(linkedin, facebook), nrow = 2, byrow = TRUE)
+
+# When does views equal 13?
+views == 13
+
+# When is views less than or equal to 14?
+views <= 14
