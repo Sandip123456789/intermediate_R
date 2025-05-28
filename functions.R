20b6ab021c3eaea80cb110aba88458ea0c5bdfc7
Sandip chaudhary <sndp.c114@gmail.com>
2025-05-28 20:37:39 +0200

    few updates

---
+++ functions.R
@@ -0,0 +1,29 @@
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
+
+# Calculate the mean of the sum
+avg_sum <- mean(linkedin + facebook)
+
+# Calculate the trimmed mean of the sum
+avg_sum_trimmed <- mean(linkedin + facebook, trim = 0.2)
+
+# Inspect both new variables
+avg_sum
+avg_sum_trimmed
+
+#******************************************************************************
+
+'Use a Function'
+
+# The linkedin and facebook vectors have already been created for you
+linkedin <- c(16, 9, 13, 5, NA, 17, 14)
+facebook <- c(17, NA, 5, 16, 8, 13, 14)
+
+# Basic average of linkedin
+mean(linkedin)
+#mean(facebook)
+
+# Advanced average of linkedin
+mean(linkedin, na.rm = TRUE)
+#mean(facebook, na.rm = TRUE)
