94280d31723754224caab9774cd6a84e8538cf43
Sandip chaudhary <sndp.c114@gmail.com>
2025-07-02 20:25:16 +0200

    1. Full duplicate (count and drop) 2. removing partial duplicates 3. aggregating partial duplicates 4. Checking Membership / Membership Constraints 5. Case Inconsistency 6. Collapsing Categories

--- cleaning_data.R
+++ cleaning_data.R
@@ -96,3 +96,146 @@ bike_share_rides_past <- bike_share_rides %>%
 assert_all_are_in_past(bike_share_rides_past$date)
 
 #*******************************************************************************
+
+'Full Duplicate (Count and Drop)'
+
+# Count the number of full duplicates
+sum(duplicated(bike_share_rides))
+
+# Remove duplicates
+bike_share_rides_unique <- distinct(bike_share_rides)
+
+# Count the full duplicates in bike_share_rides_unique
+sum(duplicated(bike_share_rides_unique))
+
+#******************************************************************************
+
+'Removing Partial Duplicates'
+
+# Find duplicated ride_ids
+bike_share_rides %>% 
+  # Count the number of occurrences of each ride_id
+  count(ride_id) %>% 
+  # Filter for rows with a count > 1
+  filter(n > 1)
+
+# Remove full and partial duplicates
+bike_share_rides_unique <- bike_share_rides %>%
+  # Only based on ride_id instead of all cols
+  distinct(ride_id, .keep_all = TRUE)
+
+# Find duplicated ride_ids in bike_share_rides_unique
+bike_share_rides_unique %>%
+  # Count the number of occurrences of each ride_id
+  count(ride_id) %>%
+  # Filter for rows with a count > 1
+  filter(n > 1)
+
+#*******************************************************************************
+
+'Aggregating Partial Duplicates'
+
+bike_share_rides %>%
+  # Group by ride_id and date
+  group_by(ride_id, date) %>%
+  # Add duration_min_avg column
+  mutate(duration_min_avg = mean(duration_min) ) %>%
+  # Remove duplicates based on ride_id and date, keep all cols
+  distinct(ride_id, date, .keep_all = TRUE) %>%
+  # Remove duration_min column
+  select(-duration_min)
+
+#*****************************************************************************
+
+'Checking Membership / Membership Constraints'
+
+'Not a member'
+
+'
+1. Use the correct type of filtering join on the sfo_survey data frame and the 
+  dest_sizes data frame to get the rows of sfo_survey with invalid dest_size values.
+2. Get the id, airline, destination, and dest_size columns.
+'
+# Find bad dest_size rows
+sfo_survey %>% 
+  # Join with dest_sizes data frame to get bad dest_size rows
+  anti_join(dest_sizes, by = "dest_size") %>%
+  # Select id, airline, destination, and dest_size cols
+  select(id, airline, destination, dest_size)
+
+'
+1. Use the correct filtering join on sfo_survey and dest_sizes to get the rows 
+  of sfo_survey that have a valid dest_size.
+2. Count the number of times that each dest_size occurs to make sure there are no
+  invalid values left behind.
+'
+# Remove bad dest_size rows
+sfo_survey %>% 
+  # Join with dest_sizes
+  semi_join(dest_sizes, by = "dest_size") %>%
+  # Count the number of each dest_size
+  count(dest_size)
+
+#******************************************************************************
+
+'*Case Inconsistency*'
+
+'Correcting/Removing inconsistency'
+
+'
+1. Add a column to sfo_survey called dest_size_trimmed that contains the values
+  in the dest_size column with all leading and trailing whitespace removed.
+2. Add another column called cleanliness_lower that contains the values in the 
+  cleanliness column converted to all lowercase.
+3. Count the number of occurrences of each category in dest_size_trimmed.
+4. Count the number of occurrences of each category in cleanliness_lower.
+'
+
+# Add new columns to sfo_survey
+sfo_survey <- sfo_survey %>%
+  # dest_size_trimmed: dest_size without whitespace
+  mutate(dest_size_trimmed = str_trim(dest_size),
+         # cleanliness_lower: cleanliness converted to lowercase
+         cleanliness_lower = str_to_lower(cleanliness))
+
+# Count values of dest_size_trimmed
+sfo_survey %>%
+  count(dest_size_trimmed)
+
+# Count values of cleanliness_lower
+sfo_survey %>%
+  count(cleanliness_lower)
+
+#***************************************************************************
+
+'Collapsing Categories'
+
+# Count categories of dest_region
+sfo_survey %>%
+  count(dest_region)
+
+'Note: Here "EU", "eur", and "Europ" need to be collapsed to "Europe".'
+
+'
+1. Create a vector called europe_categories containing the three values of 
+  dest_region that need to be collapsed.
+2. Add a new column to sfo_survey called dest_region_collapsed that contains the 
+  values from the dest_region column, except the categories stored in 
+  europe_categories should be collapsed to Europe.
+3. Count the categories of dest_region_collapsed.
+'
+
+# Count categories of dest_region
+sfo_survey %>%
+  count(dest_region)
+
+# Categories to map to Europe
+europe_categories <- c("EU", "eur", "Europ")
+
+# Add a new col dest_region_collapsed
+sfo_survey %>%
+  # Map all categories in europe_categories to Europe
+  mutate(dest_region_collapsed = fct_collapse(dest_region, 
+                                              Europe = europe_categories)) %>%
+  # Count categories of dest_region_collapsed
+  count(dest_region_collapsed)
